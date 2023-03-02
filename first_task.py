725ca25d71b155578af26ee0af4a0906eab9df05
Linda S <linda.steinberga.86@gmail.com>
2023-03-02 20:21:34 +0200

    two way

---
+++ first_task.py
@@ -0,0 +1,18 @@
+def max_number(num1, num2, num3):
+    highest_number = 0
+    numbers = [num1, num2, num3]
+    for number in numbers:
+        if number > highest_number:
+            highest_number = number
+    print(highest_number)
+    return highest_number
+
+#new
+#Write a Python function to find the Max of three numbers.
+def max_of_two( a, b ):
+    if a > b:
+        return a
+    return b
+def max_of_three( a, b, c ):
+    return max_of_two( a, max_of_two( b, c ) )
+print(max_of_three(10, 20, 30))
