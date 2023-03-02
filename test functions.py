d96387f82111328eae3dfd06674f4800d43f31c5
Linda S <linda.steinberga.86@gmail.com>
2023-03-02 19:24:50 +0200

    Function

---
+++ test functions.py
@@ -0,0 +1,19 @@
+def my_function():
+    print('hello world')
+
+print('=======================================')
+my_function()
+
+#new
+def my_function():
+    return 6
+
+print('=======================================')
+print(my_function())
+
+#new
+def my_function(number):
+    return('Value is:' + str(number))
+
+print(my_function(10))
+
